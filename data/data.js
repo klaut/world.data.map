22653fde294f9c0f44862ac4af8b523448359c47
Tanja Pislar <tanja.pislar@gmail.com>
2012-01-16 13:53:14 +0000

    better page

---
+++ data/data.js
@@ -0,0 +1,10 @@
+window.cities = [
+	{city: 'Edinburgh', latitude:-3.13, longitude:55.57, value: 10},
+	{city: 'San Francisco', longitude: 37.6, latitude: -122.3, value: 150},
+	{city: 'New York', longitude: 40.7, latitude: -74.0, value: 250},
+	{longitude:47, latitude:-122, city:"Seattle", value:15},
+	{longitude:-23.5, latitude:-46.6, city:"Sao Paulo", value:30},
+	{longitude:51.5, latitude:-0.1, city:"London", value:130},
+	{longitude:-37.8, latitude:145.0, city:"Melbourne", value:112},
+	{longitude:35.685, latitude:139.75, city:"Tokyo", value:78}
+]
